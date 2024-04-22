0a25894c910faf83f3dacd884c71ce8953452e00
Can NAMLI <namlimuhammetcan55@gmail.com>
2024-04-22 09:25:20 +0300

    Instead of "math/rand" library, the more secure "crypto/rand" library was imported. Error feedback has been added to notify the user of some undesirable situations. It was decided to create passwords of 2 different lengths, depending on the user's request.

--- pass_gen.go
+++ pass_gen.go
@@ -1,28 +1,47 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
-	"time"
+	"math/big"
 )
 
 func main() {
-	// Generate a seed for randomness
-	rand.Seed(time.Now().UnixNano())
+	// Generate passwords with different lengths
+	password1, err := generatePassword(12)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	// Call the password generator function and create a 12-character password
-	password := generatePassword(12)
-	fmt.Println("Oluşturulan Parola:", password)
+	password2, err := generatePassword(16)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Generated Password 1:", password1)
+	fmt.Println("Generated Password 2:", password2)
 }
 
-func generatePassword(length int) string {
+func generatePassword(length int) (string, error) {
+	if length <= 0 {
+		return "", fmt.Errorf("password length must be greater than 0")
+	}
+
 	// Defining password characters
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+"
 
 	// Select random characters by looping through password length
 	password := make([]byte, length)
+
 	for i := range password {
-		password[i] = charset[rand.Intn(len(charset))]
+		// Generate a random index within the charset length using crypto/rand
+		randomIndex, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		password[i] = charset[randomIndex.Int64()]
 	}
-	return string(password)
+	return string(password), nil
 }
